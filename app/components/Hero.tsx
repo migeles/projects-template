cea5e5115e0512ed73f969a83fda4e72342aab9e
migeles <kikjidetes@gmail.com>
2026-01-22 23:07:58 +0700

    npm run dev now work at first init

---
+++ app/components/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <div className="w-full h-screen  bg-zinc-50 font-sans dark:bg-black main-font">
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="text-white text-7xl">Hello</h1>
+      </div>
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="text-white text-7xl">My future self</h1>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen  bg-zinc-50 font-sans dark:bg-black">
-     <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-white text-7xl">Hello</h1>
-     </div>
-     <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-white text-7xl">My future self</h1>
-     </div>
+    <div className="w-full h-screen  bg-zinc-50 font-sans dark:bg-black main-font">
+     <Hero/>
     </div>
   );
 }
